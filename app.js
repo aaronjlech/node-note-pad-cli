0ed759136cb47f3460dfe3a8fbee6d71e2a265ae
Aaron LeCheminant <Aaron@Brelyns-MacBook-Pro-2.local>
2017-05-29 14:21:35 -0400

    adding crud methods

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 console.log('app startinn');
-
-
 const fs = require('fs');
 const os = require('os');
+const yargs = require('yargs');
+
+
 const notes = require('./notes');
 
-var user = os.userInfo();
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err){
-   if(err) {
-      console.log('unable to write to console!')
-   }
-})
+
+
+const argv = yargs.argv;
+var command = process.argv[2];
+
+
+console.log('Command', command);
+console.log('Process', process.argv);
+console.log("yargs", argv);
+
+
+
+
+switch (command) {
+   case 'add':
+      notes.addNote(argv.title, argv.body);
+      break;
+   case "list":
+      notes.getAll();
+      break;
+   case "read":
+      notes.readNote();
+      break;
+      case "remove":
+         notes.removeNote();
+         break;
+   default:
+      console.log('Command not recognized');
+      break;
+}
--- notes.js
+++ notes.js
@@ -1,11 +1,23 @@
-console.log('qwowhwho');
-
-module.exports.addNote = () => {
+const addNote = (title, body) => {
    console.log('addNote');
    return "New Note";
+};
+
+const getAll = () => {
+   console.log('getting all notes');
+};
+
+const removeNote = (title) => {
+   console.log('removing note');
+};
+const readNote = (title) => {
+   console.log('reading note');
 }
 
+module.exports = {
+   addNote,
+   getAll,
+   removeNote,
+   readNote
 
-module.exports.add = (num) => {
-   return num;    
 }
